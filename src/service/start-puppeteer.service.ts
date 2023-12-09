851c83db564b00f9e055e2471689c2e6931447a9
rprojetos.ti@hotmail.com <rprojetos.ti@hotmail.com>
2023-12-09 12:15:52 -0300

    criando o metodo fileGenerator

--- src/service/start-puppeteer.service.ts
+++ src/service/start-puppeteer.service.ts
@@ -1,8 +1,11 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { IFileGenerator } from 'interface/file-generator.interface';
 import { resolve } from 'path';
 import puppeteer, { Page } from 'puppeteer';
 
 class StartPuppeteeerSevice {
-  constructor() {}
+  constructor() { }
 
   public start(url: string): Promise<Page> {
     return new Promise(async (resolve, reject) => {
@@ -18,6 +21,16 @@ class StartPuppeteeerSevice {
       return resolve(page);
     });
   }
+
+  public fileGenerator(payload: Array<IFileGenerator>, fileName: string) {
+    const pathTmp = 'tmp';
+    if (!fs.existsSync(path.resolve(pathTmp))) {
+      console.log(`Criando o diretório ${path.resolve(pathTmp)}`);
+      fs.mkdirSync(path.resolve(pathTmp));
+    }
+    console.log(`O diretório ${path.resolve(pathTmp)} já existe`);
+  }
+
 }
 
 export const startPuppeteeerSevice = new StartPuppeteeerSevice();
